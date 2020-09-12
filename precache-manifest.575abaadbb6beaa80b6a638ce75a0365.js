dd89037f30964cf310a61e28f53d6eebec204074
atifaiman <alserembani94@gmail.com>
2020-09-13 00:40:01 +0800

    Updates

---
+++ precache-manifest.575abaadbb6beaa80b6a638ce75a0365.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bafbc8dbb1cbaebb636255420bd36ec0",
+    "url": "/neumorph/index.html"
+  },
+  {
+    "revision": "598035fe906314572a6d",
+    "url": "/neumorph/static/css/main.38dfb6c2.chunk.css"
+  },
+  {
+    "revision": "039aa0b1aaec5bb5d790",
+    "url": "/neumorph/static/js/2.d0c084f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/neumorph/static/js/2.d0c084f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "598035fe906314572a6d",
+    "url": "/neumorph/static/js/main.c60d198f.chunk.js"
+  },
+  {
+    "revision": "46a26c6ea2209a437625",
+    "url": "/neumorph/static/js/runtime-main.1f6fc0b2.js"
+  }
+]);
